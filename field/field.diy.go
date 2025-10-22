7329357eac6e888365ebcf11efd61f096c7e0c46
ShuQingZai <overbeck.jack@outlook.com>
2025-10-22 18:03:17 +0800

    refactor(field): 重构泛型字段方法返回值
    - 将 Add、Sub、Mul、Div、Mod、FloorDiv 和 Floor 方法的返回值从直接构造改为使用 newGenerics 函数
    - 统一 Between 方法中的类型断言为 any 类型
    - 提高代码可维护性和一致性

--- field/field.diy.go
+++ field/field.diy.go
@@ -2,7 +2,7 @@ package field
 
 // Between ...
 func (field genericsField[T]) Between(left T, right T) Expr {
-	return field.between([]interface{}{left, right})
+	return field.between([]any{left, right})
 }
 
 // NotBetween ...
@@ -12,35 +12,35 @@ func (field genericsField[T]) NotBetween(left T, right T) Expr {
 
 // Add ...
 func (field genericsField[T]) Add(value T) genericsField[T] {
-	return genericsField[T]{field.add(value)}
+	return newGenerics[T](field.add(value))
 }
 
 // Sub ...
 func (field genericsField[T]) Sub(value T) genericsField[T] {
-	return genericsField[T]{field.sub(value)}
+	return newGenerics[T](field.sub(value))
 }
 
 // Mul ...
 func (field genericsField[T]) Mul(value T) genericsField[T] {
-	return genericsField[T]{field.mul(value)}
+	return newGenerics[T](field.mul(value))
 }
 
 // Div ...
 func (field genericsField[T]) Div(value T) genericsField[T] {
-	return genericsField[T]{field.div(value)}
+	return newGenerics[T](field.div(value))
 }
 
 // Mod ...
 func (field genericsField[T]) Mod(value T) genericsField[T] {
-	return genericsField[T]{field.mod(value)}
+	return newGenerics[T](field.mod(value))
 }
 
 // FloorDiv ...
 func (field genericsField[T]) FloorDiv(value T) genericsField[T] {
-	return genericsField[T]{field.floorDiv(value)}
+	return newGenerics[T](field.floorDiv(value))
 }
 
 // Floor ...
 func (field genericsField[T]) Floor() genericsField[T] {
-	return genericsField[T]{field.floor()}
+	return newGenerics[T](field.floor())
 }
